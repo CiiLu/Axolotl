d630a8db23a113fe6bca291e2eebca2b84329c94
Lintern <Mr.Yu_Ze@Outlook.com>
2026-08-16 15:15:09 +0800

    refactor(app): 移除Grid 布局和List 布局卡片上的作者显示

--- packages/ui/src/components/project/card/ProjectCard.vue
+++ packages/ui/src/components/project/card/ProjectCard.vue
@@ -40,7 +40,6 @@
 									>
 										{{ provider === 'curseforge' ? 'CurseForge' : 'Modrinth' }}
 									</span>
-									<ProjectCardAuthor v-if="author" :author="author" />
 									<ProjectStatusBadge v-if="status" :status="status" class="text-sm" />
 								</div>
 								<div class="m-0 font-normal line-clamp-2">
@@ -132,7 +131,6 @@
 					>
 						{{ provider === 'curseforge' ? 'CurseForge' : 'Modrinth' }}
 					</span>
-					<ProjectCardAuthor v-if="author" :author="author" />
 					<ProjectStatusBadge v-if="status" :status="status" />
 				</div>
 				<div class="project-card-summary m-0 font-normal line-clamp-2">
